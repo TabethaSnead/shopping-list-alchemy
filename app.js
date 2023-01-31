9b538c59501038cb2fcab8b2a03094760c520792
TabethaSnead <tabetha.snead@gmail.com>
2023-01-31 12:32:05 -0800

    grabbed elements from DOM in javascript

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 /* Imports */
+import { renderListItems } from './render-utils.js';
+import { signInUser, signUpUser, signOutUser, getUser } from './fetch-utils.js';
 // this will check if we have a user and set signout link if it exists
-import './auth/user.js';
 
 /* Get DOM Elements */
-
+const shoppingListForm = document.getElementById('shopping-list-form');
+const itemsEl = document.getElementById('items-div');
+const deleteButton = document.getElementById('delete-button');
+const logoutButton = document.getElementById('logout-button');
 /* State */
 
 /* Events */
